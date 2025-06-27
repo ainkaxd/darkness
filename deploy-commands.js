9422bf30bfc820491461bffcb1b910989fa2f664
ainkaxd <ain.kaaa111@gmail.com>
2025-06-27 18:54:33 +0500

    Обновление команд и настроек .env

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const { REST, Routes } = require('discord.js');
-const { clientId, guildId, token } = require('./config.json');
 const fs = require('fs');
+
+const { CLIENT_ID, GUILD_ID, TOKEN } = process.env;
 
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -10,15 +12,15 @@ for (const file of commandFiles) {
   commands.push(command.data.toJSON());
 }
 
-const rest = new REST({ version: '10' }).setToken(token);
+const rest = new REST({ version: '10' }).setToken(TOKEN);
 
 (async () => {
   try {
     console.log('🔁 Обновление слэш-команд...');
 
     await rest.put(
-      Routes.applicationGuildCommands(clientId, guildId),
-      { body: commands },
+      Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
+      { body: commands }
     );
 
     console.log('✅ Команды успешно обновлены.');
